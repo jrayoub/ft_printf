02196fd0b8060a754d7b3cb11ccec6d62cd67e62
jrayoub <aitounaayoub05@gmail.com>
2022-10-28 01:57:38 +0100

    ft_printf

---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaitouna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 07:59:26 by aaitouna          #+#    #+#             */
+/*   Updated: 2022/10/15 07:59:28 by aaitouna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <limits.h>
+# include <stdint.h>
+# include <unistd.h>
+
+size_t				ft_strlen(const char *c);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putnbr_fd(int n, int fd);
+void				ft_putstr_fd(char *s, int fd);
+
+#endif
